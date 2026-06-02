bd845bf1da9cac72defaec84a916e3695f18effe
Adarsh Dhar <dharadarsh0@gmail.com>
2026-06-03 04:39:52 +0530

    minor 2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  allowedDevOrigins: ['192.168.29.73'],
   experimental: {
     instrumentationHook: true,
   },
